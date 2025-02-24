7f0dc864478e9f3f575638fc1c9cd5c6ab7314b4
Ian Hamilton <ian@colorcrate.com>
2025-02-24 13:49:23 -0500

    Add custom block categories

--- app/filters.php
+++ app/filters.php
@@ -14,3 +14,23 @@ namespace App;
 add_filter('excerpt_more', function () {
     return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Continued', 'sage'));
 });
+
+/**
+ * Custom block categories
+ */
+add_filter( 'block_categories_all' , function( $categories ) {
+    // WordPress orders the items in the listing based on their position
+    // in the array, so if we want to be at the front of the list we
+    // want to 'unshift' to put our categories at the start.
+    array_unshift( $categories,
+        array(
+            'slug'  => 'cc-header',
+            'title' => 'CC Headers'
+        ),
+        array(
+            'slug'  => 'cc-body',
+            'title' => 'CC Body Blocks'
+        )
+    );
+    return $categories;
+});
